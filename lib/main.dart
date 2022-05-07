ad47da4e34b92509375bb748b555c120db13b82e
Danish <mahmooddanish005@gmail.com>
2022-05-07 16:03:22 +0200

    newbranc

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // debugShowCheckedModeBanner: false,
+        // debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('applictaion bar'),
-            backgroundColor: Colors.red[600],
-
-          ),
-
-        )
-    );
+      appBar: AppBar(
+        title: Text('applictaion bar'),
+        backgroundColor: Colors.green[600],
+      ),
+    ));
   }
 }
-
