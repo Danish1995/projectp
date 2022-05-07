6107a9e8fbd69846f18c63389c32ecf874272294
Danish <mahmooddanish005@gmail.com>
2022-05-07 16:09:45 +0200

    newbrac3

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text('applictaion bar'),
+        title: Text(' bar'),
         backgroundColor: Colors.green[600],
       ),
     ));
